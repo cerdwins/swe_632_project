353b495665fe6c19ff330088110c9a0d4744db96
Chris Erdwins <erdwinsc@gmail.com>
2018-02-08 17:03:13 -0500

    Took the static functions out of the document.ready function as they are static and adding some functionality

--- Js/todo.js
+++ Js/todo.js
@@ -3,35 +3,23 @@ $(document).ready(function() {
     //updates the categorized very high area and changes the classes 
     $("#todo-list-very").click(function(){
         getDataForCategorized("veryhigh");
-        this.first.addClass("fa-caret-right");
-        this.first.removeClass("fa-caret-down");
+        this.firstChild.className = "fa fa-caret-right";
     });
 
     //updates the categorized high area
     $("#todo-list-high").click(function(){
         getDataForCategorized("high");
-        this.first.addClass("fa-caret-right");
-        this.first.removeClass("fa-caret-down");
+        this.firstChild.className = "fa fa-caret-right";
     });
 
     //updates the categorized normal area
     $("#todo-list-normal").click(function(){
         getDataForCategorized("normal");
-        this.first.addClass("fa-caret-right");
-        this.first.removeClass("fa-caret-down");
+        this.firstChild.className = "fa fa-caret-right";
     });
     
-    //Creates the line items under a given section of the sort area
-    function getDataForCategorized(categorization){
-        var data = [];
-        data = searchByImportance(categorization);
-        $("#todo-list-" + categorization +"-hiddenlist").html("<ul id='" + categorization + "-newlist'></ul>");
-        for(x = 0; x < data.length; x++){
-            var listItem = "<li></li>";
-            $(categorization + "-newlist").append(listItem);
-        }
-    }
     
+
     // create Calendar from div HTML element
     $("#mainCalendar").kendoCalendar({
         format: "MM/dd/yyyy",
@@ -48,131 +36,7 @@ $(document).ready(function() {
         // display month and year in the input
         format: "MM/dd/yyyy",
     });
-    //Class to create todo Object
-    function Todo(id, name, dueDate, importance, isCompleted) {
-        this.id = id;
-        this.name = name;
-        this.dueDate = dueDate;
-        this.importance = importance;
-        this.isCompleted = isCompleted;
-    }
-    //Class declaration ends
-    /****************LOCAL STORAGE STUFFS***************/
-    if (typeof(Storage) !== "undefined") {
-
-    } else {
-        alert("Local storage not supported.")
-    }
-    /****************LOCAL STORAGE STUFFS ENDS***************/
-    //stored using the key="todos"
-    function getDataFromLocalStorage() {
-        return JSON.parse(localStorage.getItem("todos"));
-    }
-    //stored using the key="todos"
-    function setDataToLocalStorage(data) {
-        if (data) {
-            localStorage.setItem("todos", JSON.stringify(data));
-        }
-    }
-    //Users CRUD operations
-    function addToDoList(name, dueDate, importance, isCompleted) {
-        //get the local storage data // get the index of the highest item // add to the list //set the localstorage
-        currentData = getDataFromLocalStorage();
-
-        if (!currentData) {
-            var currentData = {
-                index: 0,
-                items: []
-            };
-            currentData.index += 1;
-            var toDo = new Todo(currentData.index, name, dueDate, importance, isCompleted);
-            currentData.items.push(toDo);
-            setDataToLocalStorage(currentData);
-        } else {
-            currentData.index += 1;
-            var toDo = new Todo(currentData.index, name, dueDate, importance, isCompleted);
-            currentData.items.push(toDo);
-            setDataToLocalStorage(currentData);
-        }
-
-    }
-    //Display all the items
-    function showData() {
-        return getDataFromLocalStorage();
-    }
-    //change from completedToNotCompletedAndViceVersa
-    function changeStatusOfAToDo(id) {
-        //Search for the items
-        var currentData = showData();
-        if (currentData) {
-            //Check to see if there is any item in the storage
-            if (currentData.index > 0) {
-                if (currentData.items[id - 1].isCompleted) {
-                    currentData.items[id - 1].isCompleted = false;
-                } else {
-                    currentData.items[id - 1].isCompleted = true;
-                }
-                setDataToLocalStorage(currentData);
-            }
-        }
-
-    }
-    //search for an item by importance
-    function searchByImportance(importance) {
-        var result = [];
-        var currentData = showData();
-        if (currentData) {
-            if (currentData.index > 0) {
-                result = currentData.items.filter(filterByImportance(currentData.items, importance.toLowerCase()));
-            }
-        }
-        return result;
-    }
-
-    //utility for importance search
-    function filterByImportance(data, importance) {
-        if (importance == "normal") {
-            return data.importance == "normal";
-        } else if (importance == "high") {
-            return data.importance == "high";
-        } else if (importance == "veryhigh") {
-            return data.importance == "veryhigh";
-        } else {
-            return data;
-        }
-
-    }
-    //search by dates
-    function searchByDates(fromDate, toDate) {
-        var result = [];
-        //not implemented yet
-        return result;
-    }
-    //Delete item from the storage
-    function deleteItem(id) {
-        var currentData = showData();
-        if (currentData) {
-            if (currentData.index == 1) {
-                localStorage.clear();
-            } else {
-                currentData.index -= 1;
-                currentData.items = removeItemFromArray(currentData.items, currentData.index);
-                setDataToLocalStorage(currentData);
-            }
-
-        }
-    }
-    //Utility methods- this will return a new array
-    function removeItemFromArray(array, index) {
-        return array.filter(e => e !== array[index]);
-    }
-
-    /*************DATE UTILITIES************/
-    //convert to mm/dd/yyyy
-    function toMMDDYYYY(date) {
-        var dateInMMDDYYYY = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
-        return dateInMMDDYYYY;
-    }
+    
 
     /******************EVENT HANDLING PARTS ONLY********************** */
 
@@ -180,16 +44,8 @@ $(document).ready(function() {
     var calendar = $("#mainCalendar").data('kendoCalendar');
     var dateSelected = calendar.current();
     displayCalendarValue(toMMDDYYYY(dateSelected));
-    //to Display the value of the calendar on the text
-    function displayCalendarValue(val) {
-        var selecteDateId = "#selectedDate";
-        $(selecteDateId).text(val);
-    }
-    //When calendar value changes, change the dates as well
-    function calendarChange() {
-        dateSelected = toMMDDYYYY(this.value());
-        displayCalendarValue(dateSelected)
-    }
+
+
 
     //When an item is created
     $('#createForm').submit(function(event) {
@@ -206,3 +62,152 @@ $(document).ready(function() {
 
  
 });
+
+
+
+//Creates the line items under a given section of the sort area
+function getDataForCategorized(categorization){
+    var data = [];
+    data = searchByImportance(categorization);
+    $("#todo-list-" + categorization +"-hiddenlist").html("<ul id='" + categorization + "-newlist'></ul>");
+    for(x = 0; x < data.length; x++){
+        var listItem = "<li></li>";
+        $(categorization + "-newlist").append(listItem);
+    }
+}
+
+//to Display the value of the calendar on the text
+function displayCalendarValue(val) {
+    var selecteDateId = "#selectedDate";
+    $(selecteDateId).text(val);
+}
+//When calendar value changes, change the dates as well
+function calendarChange() {
+    dateSelected = toMMDDYYYY(this.value());
+    displayCalendarValue(dateSelected)
+}
+
+//Class to create todo Object
+function Todo(id, name, dueDate, importance, isCompleted) {
+    this.id = id;
+    this.name = name;
+    this.dueDate = dueDate;
+    this.importance = importance;
+    this.isCompleted = isCompleted;
+}
+//Class declaration ends
+/****************LOCAL STORAGE STUFFS***************/
+if (typeof(Storage) !== "undefined") {
+
+} else {
+    alert("Local storage not supported.")
+}
+/****************LOCAL STORAGE STUFFS ENDS***************/
+//stored using the key="todos"
+function getDataFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("todos"));
+}
+//stored using the key="todos"
+function setDataToLocalStorage(data) {
+    if (data) {
+        localStorage.setItem("todos", JSON.stringify(data));
+    }
+}
+//Users CRUD operations
+function addToDoList(name, dueDate, importance, isCompleted) {
+    //get the local storage data // get the index of the highest item // add to the list //set the localstorage
+    currentData = getDataFromLocalStorage();
+
+    if (!currentData) {
+        var currentData = {
+            index: 0,
+            items: []
+        };
+        currentData.index += 1;
+        var toDo = new Todo(currentData.index, name, dueDate, importance, isCompleted);
+        currentData.items.push(toDo);
+        setDataToLocalStorage(currentData);
+    } else {
+        currentData.index += 1;
+        var toDo = new Todo(currentData.index, name, dueDate, importance, isCompleted);
+        currentData.items.push(toDo);
+        setDataToLocalStorage(currentData);
+    }
+}
+//Display all the items
+function showData() {
+    return getDataFromLocalStorage();
+}
+//change from completedToNotCompletedAndViceVersa
+function changeStatusOfAToDo(id) {
+    //Search for the items
+    var currentData = showData();
+    if (currentData) {
+        //Check to see if there is any item in the storage
+        if (currentData.index > 0) {
+            if (currentData.items[id - 1].isCompleted) {
+                currentData.items[id - 1].isCompleted = false;
+            } else {
+                currentData.items[id - 1].isCompleted = true;
+            }
+            setDataToLocalStorage(currentData);
+        }
+    }
+
+}
+//search for an item by importance
+function searchByImportance(importance) {
+    var result = [];
+    var currentData = showData();
+    if (currentData) {
+        if (currentData.index > 0) {
+            result = currentData.items.filter(filterByImportance(currentData.items, importance.toLowerCase()));
+        }
+    }
+    return result;
+}
+
+//utility for importance search
+function filterByImportance(data, importance) {
+    if (importance == "normal") {
+        return data.importance == "normal";
+    } else if (importance == "high") {
+        return data.importance == "high";
+    } else if (importance == "veryhigh") {
+        return data.importance == "veryhigh";
+    } else {
+        return data;
+    }
+
+}
+//search by dates
+function searchByDates(fromDate, toDate) {
+    var result = [];
+    //not implemented yet
+    return result;
+}
+//Delete item from the storage
+function deleteItem(id) {
+    var currentData = showData();
+    if (currentData) {
+        if (currentData.index == 1) {
+            localStorage.clear();
+        } else {
+            currentData.index -= 1;
+            currentData.items = removeItemFromArray(currentData.items, currentData.index);
+            setDataToLocalStorage(currentData);
+        }
+
+    }
+}
+//Utility methods- this will return a new array
+function removeItemFromArray(array, index) {
+    return array.filter(e => e !== array[index]);
+}
+
+/*************DATE UTILITIES************/
+//convert to mm/dd/yyyy
+function toMMDDYYYY(date) {
+    var dateInMMDDYYYY = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+    return dateInMMDDYYYY;
+}
